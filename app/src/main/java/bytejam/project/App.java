6b45481afd9e54b113e809e8a7cf94e2fcb69bd6
BacconMaster1337 <lucasmcguire09@gmail.com>
2024-09-21 14:44:01 -0500

    this was a test did nothing

--- app/src/main/java/bytejam/project/App.java
+++ app/src/main/java/bytejam/project/App.java
@@ -11,5 +11,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.println("your moms gay");
+        System.out.println("this is a test");
+
     }
 }
