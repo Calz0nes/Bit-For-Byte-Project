14c069fd51a6ac53c273d5e302e612ae7682065d
Calz0nes <cadelconner@gmail.com>
2024-10-26 12:01:52 -0500

    working ish

--- app/src/main/java/bytejam/project/turbo/PresentScene.java
+++ app/src/main/java/bytejam/project/turbo/PresentScene.java
@@ -1,17 +1,70 @@
 package bytejam.project.turbo;
 
+import org.joml.Vector2f;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_A;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_D;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_S;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
+
+import bytejam.project.renderer.Renderer;
+import bytejam.project.turbo.game_objects.Background;
+import bytejam.project.turbo.game_objects.Player;
+import bytejam.project.turbo.util.AssetPool;
+import bytejam.project.turbo.util.Transform;
+
 public class PresentScene extends Scene{
 
-    @Override
-    public void update(float dt) {
+    private Background background;
+    private Renderer renderer;
+    private Player player;
+    private Vector2f Pose;
+    private Sound sound;
+    private final float Speed = 10;
+
+    public PresentScene() {
         
     }
 
     @Override
     public void init() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init'");
+        this.camera = new Camera(new Vector2f(0, 0));
+        this.Pose = new Vector2f(0, 0);
+
+        this.renderer = new Renderer();
+        this.player = new Player(AssetPool.getTexture("assets/images/snail_07.png"), new Transform(new Vector2f(50, 50)));
+        this.background = new Background(AssetPool.getTexture("assets/images/GameBackground.jpg"), new Transform(new Vector2f(20, -400),new Vector2f(1200, 2100)));
+        
+        sound = new Sound("assets/sounds/file_example_OOG_1MG.ogg", true);
+        renderer.add(background);
+        renderer.add(player);
     }
+
+    @Override
+    public void update(float dt) {
+
+        sound.play();
+        
+        if (KeyListener.isKeyPressed(GLFW_KEY_W)) {
+            Pose.y += Speed;
+        } 
+        
+        if (KeyListener.isKeyPressed(GLFW_KEY_A)) {
+            Pose.x -= Speed;
+        } 
+        
+        if (KeyListener.isKeyPressed(GLFW_KEY_S)) {
+            Pose.y -= Speed;
+        } 
+        
+        if (KeyListener.isKeyPressed(GLFW_KEY_D)) {
+            Pose.x += Speed;
+        }
+
+        player.setPos(this.Pose);
+
+        renderer.render();
+    }
+
 
     
 
--- app/src/main/java/bytejam/project/turbo/TitleScene.java
+++ app/src/main/java/bytejam/project/turbo/TitleScene.java
@@ -1,6 +1,7 @@
 package bytejam.project.turbo;
 
 import org.joml.Vector2f;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
 
 import bytejam.project.renderer.Renderer;
 import bytejam.project.turbo.game_objects.Player;
@@ -9,9 +10,10 @@ import bytejam.project.turbo.util.Transform;
 
 public class TitleScene extends Scene{
     public Player player;
+    public Renderer renderer;
 
     public TitleScene() {
-
+        
     }
 
     @Override
@@ -33,6 +35,10 @@ public class TitleScene extends Scene{
 
     @Override
     public void update(float dt) {
+
+        if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+            Window.changeScene(0);
+        }
         // Render new frame.
         this.renderer.render();
     }
